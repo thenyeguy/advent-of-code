ab6dbb4b1318643b71a5ed8c193297ba87195dbf
Michael Nye <thenyeguy@gmail.com>
2022-12-02 19:46:43 -0800

    2022 Day 1 \o/

---
+++ 2022/01.jl
@@ -0,0 +1,27 @@
+module Day1
+
+function load_data()
+    data = split(read(joinpath(@__DIR__, "01.txt"), String), "\n\n")
+    return map(e -> parse.(Int, split(e, "\n")), data)
+end
+
+function part_a(data)
+    return data .|> sum |> maximum
+end
+
+function part_b(data)
+    sorted = sort(sum.(data), rev=true)
+    return sum(sorted[1:3])
+end
+
+function main()
+    data = load_data()
+    println("Part A: ", part_a(data))
+    println("Part B: ", part_b(data))
+end
+
+end # module Day1
+
+if abspath(PROGRAM_FILE) == @__FILE__
+    Day1.main()
+end
