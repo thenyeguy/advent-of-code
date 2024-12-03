d7f2b3237bc7a121c3361b4db9a9fff59d039806
Michael Nye <thenyeguy@gmail.com>
2024-12-02 22:20:38 -0800

    2024 Day 1

---
+++ 2024/day01.ml
@@ -0,0 +1,34 @@
+open Utils
+open Utils.List.Infix
+
+(*
+ * Parse input
+ *)
+let parse_line line =
+  let [ l; _; _; r ] = String.split_on_char ' ' line in
+  (int_of_string l, int_of_string r)
+
+let puzzle_input = Io.read_lines "2024/data/01.txt" ||> parse_line |> List.split
+
+(*
+ * Part 1
+ *)
+let distances ls rs =
+  let distance l r = Int.abs (l - r) in
+  List.map2 distance (List.sort compare ls) (List.sort compare rs)
+
+let similarity ls rs =
+  let count l = List.count (( = ) l) rs * l in
+  ls ||> count |> List.sum
+
+let part_one input = input |> Pair.apply distances |> List.sum
+
+(*
+ * Part 2
+ *)
+let part_two input = input |> Pair.apply similarity
+
+(*
+ * Main
+ *)
+let _ = Runner.main puzzle_input part_one part_two
