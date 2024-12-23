4d6be41abf53acaedf78385ab8b93d2a75d3bc43
Michael Nye <thenyeguy@gmail.com>
2024-12-22 16:06:04 -0800

    2024 Day 21

---
+++ 2024/day21.ml
@@ -0,0 +1,91 @@
+open Utils
+open Utils.Coord.Infix
+open Utils.List.Infix
+
+(*
+ * Parse input
+ *)
+let puzzle_input = Io.read_lines "2024/data/21.txt"
+
+(*
+ * Part 1
+ *)
+let key_coord (key : char) : Coord.t =
+  match key with
+  (* 7 8 9
+   * 4 5 6
+   * 1 2 3
+   *   0 A *)
+  | 'A' -> (0, 0)
+  | '0' -> (0, 1)
+  | '1' -> (1, 2)
+  | '2' -> (1, 1)
+  | '3' -> (1, 0)
+  | '4' -> (2, 2)
+  | '5' -> (2, 1)
+  | '6' -> (2, 0)
+  | '7' -> (3, 2)
+  | '8' -> (3, 1)
+  | '9' -> (3, 0)
+  (*   ^ A
+   * < v > *)
+  | '^' -> (0, 1)
+  | '<' -> (-1, 2)
+  | 'v' -> (-1, 1)
+  | '>' -> (-1, 0)
+
+let rec paths (h : char) (h_count : int) (v : char) (v_count : int) :
+    char list list =
+  match (h_count, v_count) with
+  | 0, 0 -> [ [ 'A' ] ]
+  | n, 0 -> [ List.repeated h n @ [ 'A' ] ]
+  | 0, n -> [ List.repeated v n @ [ 'A' ] ]
+  | _ ->
+      List.map (List.cons h) (paths h (h_count - 1) v v_count)
+      @ List.map (List.cons v) (paths h h_count v (v_count - 1))
+
+let valid_path (src : char) (path : char list) =
+  match (src, path) with
+  | 'A', '<' :: '<' :: _ -> false
+  | '0', '<' :: _ -> false
+  | '^', '<' :: _ -> false
+  | '<', '^' :: _ -> false
+  | '1', 'v' :: _ -> false
+  | '4', 'v' :: 'v' :: _ -> false
+  | '7', 'v' :: 'v' :: 'v' :: _ -> false
+  | _ -> true
+
+let button_presses self ((robot, keys) : int * char list) : int =
+  let press (dst : char) (src : char) : int =
+    let rows, cols = key_coord dst -- key_coord src in
+    let h, h_count = if cols < 0 then ('>', -cols) else ('<', cols) in
+    let v, v_count = if rows < 0 then ('v', -rows) else ('^', rows) in
+    paths h h_count v v_count
+    |> List.filter (valid_path src)
+    ||> (if robot = 1 then List.length else fun p -> self (robot - 1, p))
+    |> List.min
+  in
+  List.pair_map press ('A' :: keys) |> List.sum
+
+let button_presses_memo = Memo.memo_rec button_presses
+
+let complexity (robots : int) (keys : string) : int =
+  let presses = button_presses_memo (robots, String.explode keys) in
+  let code = String.sub keys 0 3 |> int_of_string in
+  presses * code
+
+let complexities ~(robots : int) (codes : string list) : int =
+  let acc total code = total + complexity robots code in
+  List.fold_left acc 0 codes
+
+let part_one = complexities ~robots:3
+
+(*
+ * Part 2
+ *)
+let part_two = complexities ~robots:26
+
+(*
+ * Main
+ *)
+let _ = Runner.main puzzle_input part_one part_two
--- ocaml_utils/string.ml
+++ ocaml_utils/string.ml
@@ -3,6 +3,9 @@ include Stdlib.String
 (* Converts a string into a list of its characters. *)
 let explode (s : string) : char list = to_seq s |> List.of_seq
 
+(* Converts a list characters into a string. *)
+let implode (cs : char list) : string = List.to_seq cs |> of_seq
+
 (* Folds over the string, character-by-character. *)
 let fold_left (f : 'acc -> char -> 'acc) (acc : 'acc) (s : string) : 'acc =
   explode s |> List.fold_left f acc
