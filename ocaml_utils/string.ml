48cedb02907efb6e6330d92fa9331297c64e553b
Michael Nye <thenyeguy@gmail.com>
2025-11-24 11:32:41 -0800

    Create empty 2025 directory

--- ocaml_utils/string.ml
+++ ocaml_utils/string.ml
@@ -1,5 +1,8 @@
 include Stdlib.String
 
+(* Converts a string to a list of its lines. *)
+let lines : string -> string list = split_on_char '\n'
+
 (* Converts a string into a list of its characters. *)
 let explode (s : string) : char list = to_seq s |> List.of_seq
 
